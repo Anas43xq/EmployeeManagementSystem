ffe090b17d3a4b0e5940c8c592f68204885f9692
Anas <anas@example.com>
2026-03-22 07:40:51 +0200

    update

--- src/components/login/OtpScreen.tsx
+++ src/components/login/OtpScreen.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useNotification } from '../../contexts/NotificationContext';
 import { sendLoginOtp, verifyLoginOtp } from '../../services/loginAttempts';
-import { Mail, Clock, Info } from 'lucide-react';
+import { Mail, Clock } from 'lucide-react';
 
 interface OtpScreenProps {
   email: string;
